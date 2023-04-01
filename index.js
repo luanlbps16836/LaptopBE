dfeae5393ef95a4ba49d045868777ba05b63fac5
luanlbps16836 <luanlbps16836@fpt.edu.vn>
2023-04-01 09:41:26 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const cartRoute = require("./routes/cartRoute");
+const orderRoute = require("./routes/orderRoute");
+const categoryRoute = require("./routes/categoryRoute");
+const authRoute = require("./routes/authRoute");
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("Connecting MONGOOSEDB Successfull !!!"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.use(bodyParser.json({ limit: '30mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("SERVER running PORT:", process.env.PORT);
+});
